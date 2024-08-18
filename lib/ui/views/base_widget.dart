192b1c9f69833d7251fc83e1f336baa5256949a4
sahillathwal <sahillathwal566@gmail.com>
2024-08-18 14:51:41 +0530

    adding base widget and using it in login_view

---
+++ lib/ui/views/base_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BaseWidget<T extends ChangeNotifier> extends StatefulWidget {
+  final Widget Function(BuildContext context, T value, Widget? child) builder;
+  final T model;
+  final Widget child;
+  const BaseWidget(
+      {super.key,
+      required this.model,
+      required this.builder,
+      required this.child});
+  @override
+  _BaseWidgetState<T> createState() => _BaseWidgetState<T>();
+}
+
+class _BaseWidgetState<T extends ChangeNotifier> extends State<BaseWidget<T>> {
+  late T model;
+
+  @override
+  void initState() {
+    model = widget.model;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>.value(
+      value: model,
+      child: Consumer<T>(
+        builder: widget.builder,
+        child: widget.child,
+      ),
+    );
+  }
+}
--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:provider_arc/core/constants/app_contstants.dart';
 import 'package:provider_arc/core/viewmodels/views/login_view_model.dart';
 import 'package:provider_arc/ui/shared/app_colors.dart';
+import 'package:provider_arc/ui/views/base_widget.dart';
 import 'package:provider_arc/ui/widgets/login_header.dart';
 
 class LoginView extends StatefulWidget {
@@ -17,37 +18,34 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<LoginViewModel>.value(
-      value: LoginViewModel(authenticationService: Provider.of(context)),
-      child: Consumer<LoginViewModel>(
-        child: LoginHeader(
-          controller: _controller,
-          validationMessage: '',
-        ),
-        builder: (context, model, child) => Scaffold(
-          backgroundColor: backgroundColor,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              child ?? Container(),
-              model.busy
-                  ? const CircularProgressIndicator()
-                  : TextButton(
-                      style:
-                          TextButton.styleFrom(backgroundColor: Colors.white),
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      onPressed: () async {
-                        var loginSuccess = await model.login(_controller.text);
-                        if (loginSuccess) {
-                          Navigator.pushNamed(context, RoutePaths.home);
-                        }
-                      },
-                    )
-            ],
-          ),
+    return BaseWidget<LoginViewModel>(
+      model: LoginViewModel(authenticationService: Provider.of(context)),
+      child: LoginHeader(
+        controller: _controller,
+        validationMessage: '',
+      ),
+      builder: (context, model, child) => Scaffold(
+        backgroundColor: backgroundColor,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            child ?? Container(),
+            model.busy
+                ? const CircularProgressIndicator()
+                : TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.white),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onPressed: () async {
+                      var loginSuccess = await model.login(_controller.text);
+                      if (loginSuccess) {
+                        Navigator.pushNamed(context, RoutePaths.home);
+                      }
+                    },
+                  )
+          ],
         ),
       ),
     );

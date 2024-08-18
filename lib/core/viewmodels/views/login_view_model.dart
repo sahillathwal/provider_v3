9ad71d9b71473e8691ba3ae503fd36beebef118a
sahillathwal <sahillathwal566@gmail.com>
2024-08-18 14:59:00 +0530

    adding base_model and using in login_view_model

--- lib/core/viewmodels/views/login_view_model.dart
+++ lib/core/viewmodels/views/login_view_model.dart
@@ -1,23 +1,16 @@
-import 'package:flutter/material.dart';
 import 'package:provider_arc/core/services/authentication_service.dart';
+import 'package:provider_arc/core/viewmodels/views/base_model.dart';
 
-class LoginViewModel extends ChangeNotifier {
+class LoginViewModel extends BaseModel {
   final AuthenticationService _authenticationService;
   LoginViewModel({required AuthenticationService authenticationService})
       : _authenticationService = authenticationService;
 
-  bool _busy = false;
-  bool get busy => _busy;
   Future<bool> login(String userIdText) async {
     setBusy(true);
     var userId = int.tryParse(userIdText);
     var success = await _authenticationService.login(userId!);
     setBusy(false);
     return success;
-  }
-
-  void setBusy(bool value) {
-    _busy = value;
-    notifyListeners();
   }
 }
